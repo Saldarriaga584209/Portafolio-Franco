f2714e4af9b7d7d23ae7384216b42e903bb2f508
Saldarriaga5848209 <crisalido584209@outlook.es>
2024-10-19 16:58:36 -0500

    Se agrega portafolio

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+import parallax from './components/parallax.js';
+import activeMenu from './components/selected_menu.js';
+import updateDateYear from './helpers/date_updater.js';
+import loader from './helpers/loader.js';
+import resetToHome from './helpers/reload_page.js';
+import sendEmail from './helpers/send_form.js';
+
+loader();
